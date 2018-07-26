40a0fc247e1b6a0dfc5b499a69032b3cd44a06e1
stanislavmo <stanislavmo@wix.com>
2018-07-26 11:16:07 +0300

    Add help and validation of input params to chess

--- elementary-tasks/chess/src/main/java/ssjava/chess/Main.java
+++ elementary-tasks/chess/src/main/java/ssjava/chess/Main.java
@@ -6,12 +6,32 @@ package ssjava.chess;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO handle args
-		// TODO validation
-		Grid grid = new Grid(8, 8);
+
+		final int width;
+		final int heigth;
+
+		try {
+			width = Integer.parseInt(args[0]);
+			heigth = Integer.parseInt(args[1]);
+			if (width < 1 || heigth < 1) {
+				throw new IllegalArgumentException();
+			}
+		} catch (Exception e) {
+			System.out.println("Cannot parse input params.");
+			printHelp();
+			return;
+		}
+
+		Grid grid = new Grid(width, heigth);
 		Board board = new Board(grid);
 
 		grid.printGrid();
+	}
+
+	private static void printHelp() {
+		System.out.println("Вывеодит шахматную доску с заданными шириной и высотой.\n" +
+				"Example:\n" +
+				"java ssjava.chess.Main 8 8");
 	}
 
 }
